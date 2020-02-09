b0a49480296f9e223bac43cd84d86324a17d82aa
indre101 <zygaityte.indre@gmail.com>
2020-02-09 13:31:48 +0100

    changed student house

--- script.js
+++ script.js
@@ -9,29 +9,70 @@ function init() {
     .then(data => {
       data.forEach(student => {
         createStudentCards(student, data);
+        const themeOptions = document.querySelectorAll(".theme")
+        var arr = Array.from(themeOptions);
+
+        themeOptions.forEach(e => {
+          e.addEventListener("change", () => {
+
+            data[arr.indexOf(e)].house = selected()
+            console.log(data[arr.indexOf(e)]);
+            studentCard(document.querySelectorAll(".student")[arr.indexOf(e)], data[arr.indexOf(e)])
+
+            // student.house = selected();
+            // modal.dataset.crest = selected()
+            // a.textContent = selected();
+            // b.textContent = selected();
+            // console.log(e.parentElement);
+            // console.log(data[arr.indexOf(e)].house);
+            // console.log(document.querySelectorAll(".student"));
+
+            // console.log(e.parentElement);
+            // console.log(document.querySelectorAll(".student")[data.indexOf(student)]);
+          });
+        })
       });
-    });
+
+
+
+
+    })
 }
 
-document.querySelector(".theme").addEventListener("change", selected);
-
 function selected() {
-  const selectedTheme = this.value; //... more code}
+  const selectedTheme = event.target;
+  return selectedTheme.value;
+}
+
+function changeStudentHouse() {
+  const students = document.querySelectorAll(".student")
+
 
 }
 
 function createStudentCards(student, data) {
   const clnStudent = studentTemplate.cloneNode(true);
   // console.log(student.fullname);
+
+  studentCard(clnStudent, student)
+
+  students.appendChild(clnStudent);
+}
+
+function showHideElement(element, classToAdd, classtoRemove) {
+  element.classList.toggle(`${classToAdd}`);
+  element.classList.toggle(`${classtoRemove}`);
+}
+
+
+function studentCard(clnStudent, student) {
   clnStudent.querySelector(".nameOftheStudent").textContent = student.fullname;
   clnStudent.querySelector(".house").textContent = student.house;
-  clnStudent.querySelector(".number").textContent = data.indexOf(student) + 1;
+  // clnStudent.querySelector(".number").textContent = data.indexOf(student) + 1;
   const modal = clnStudent.querySelector(".modal");
   modal.dataset.crest = student.house.toLowerCase();
   clnStudent.querySelector(".textStudentName").textContent = student.fullname
   clnStudent.querySelector(".textStudentHouse").textContent = student.house;
-
-
 
   clnStudent.querySelector(".mainStudentInfo").onclick = function () {
     showHideElement(modal, "d-flex", "d-none");
@@ -40,11 +81,4 @@ function createStudentCards(student, data) {
   clnStudent.querySelector(".close").onclick = function () {
     showHideElement(modal, "d-flex", "d-none");
   };
-
-  students.appendChild(clnStudent);
-}
-
-function showHideElement(element, classToAdd, classtoRemove) {
-  element.classList.toggle(`${classToAdd}`);
-  element.classList.toggle(`${classtoRemove}`);
 }
