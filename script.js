2884338c827c8551988122a548f014a4acb30a63
indre101 <zygaityte.indre@gmail.com>
2020-02-09 11:03:59 +0100

    removed uneccesary css

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ function createStudentCards(student, data) {
   clnStudent.querySelector(".number").textContent = data.indexOf(student) + 1;
   const modal = clnStudent.querySelector(".modal");
 
-  clnStudent.querySelector(".message").textContent = `${student.fullname} belongs to ${student.house} house`
+  clnStudent.querySelector(".textStudentName").textContent = student.fullname
+  clnStudent.querySelector(".textStudentHouse").textContent = student.house;
+
 
 
   clnStudent.querySelector(".mainStudentInfo").onclick = function () {
